b3230decd2211ccfed673db68caa8d6f0d6acfb8
José Roberto Sobrinho Lima <jsroberto.slima@gmail.com>
2018-05-05 09:37:20 -0300

    Add files via upload

--- piRNAmethods2.R
+++ piRNAmethods2.R
@@ -725,6 +725,7 @@ piRNAc <- function(CHROM) {
     auxPirnaGDF <- 
       foreach(chrom = "chr" %s+% c(1:22, "X", "Y"), .options.snow = options1,
               .combine = list, .multicombine = TRUE, .maxcombine = 24) %dopar% {
+                suppressPackageStartupMessages(require(stringi))
                 auxPirnaDir <- file.path(gitHubDir, "piRNA" %s+% chrom)
                 auxPirnaObj <- file.path(auxPirnaDir, "pirnaGDF" %s+% chrom %s+%
                                            ".rds")
